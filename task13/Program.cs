2904a1c29a3d78d03d5797799307bf7d8f4342fe
alesya <kreol_11@inbox.ru>
2022-08-10 22:15:00 +0300

    task14

--- task13/Program.cs
+++ task13/Program.cs
@@ -7,6 +7,8 @@
 32679 -> 6
 */
 
+/* 1 Ваиант
+
 Console.Write("Введи число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 string numberText = Convert.ToString(number);
@@ -19,7 +21,25 @@ else
 {
   Console.WriteLine("-> третьей цифры нет");
 }
+*/
 
+// 2 вариант
+
+Console.Write("Введи число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+string str = digit +"" ;
+
+char[] massiv = str.ToCharArray();
+
+if(massiv.Length ==0)
+{
+    Console.Write("Тртья цифра");
+}
+else
+{
+    Console.Write(massiv[2]);
+    
+}
 
 
 
---
+++ task14/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 14: Напишите программу, которая принимает
+на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 7;
+int b = 23;
+
+if(number % a == 0 && number % b == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно");
+}
